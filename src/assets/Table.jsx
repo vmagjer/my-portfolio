9554054d1b8c156a2450b6caf6365bb3df8593ea
Vlatko Magjer <vlatko.magjer@gmail.com>
2024-01-12 00:42:28 +0100

    add matrix shower effect

---
+++ src/assets/Table.jsx
@@ -0,0 +1,89 @@
+const Table = ({className}) => (
+  <svg className={className} viewBox="0 0 500 500">
+    <defs>
+      <radialGradient
+        id="gradient-2-0"
+        gradientUnits="userSpaceOnUse"
+        cx="250.497"
+        cy="441.708"
+        r="103.835"
+        spreadMethod="pad"
+        gradientTransform="matrix(1.659796, 0, 0, 1, -163.136146, 0)"
+        xlinkHref="#gradient-2"
+      />
+      <linearGradient id="gradient-2" bx:pinned="true">
+        <title>kkk</title>
+        <stop offset="0.421" style={{ stopColor: 'rgb(216, 238, 255)' }} />
+        <stop offset="0.739" style={{ stopColor: 'rgba(216, 238, 255, 0)' }} />
+        <stop offset="1" style={{ stopColor: 'rgba(0, 0, 0, 0)' }} />
+      </linearGradient>
+    </defs>
+    <path
+      d="M 87.65 410 L 412.35 410 L 485 500 L 15 500 Z"
+      style={{
+        stroke: 'rgb(0, 0, 0)',
+        strokeWidth: '0px',
+        strokeLinejoin: 'round',
+        fill: 'rgb(84, 75, 64)',
+      }}
+      transform="matrix(0.9999999999999999, 0, 0, 0.9999999999999999, 0, -2.842170943040401e-14)"
+    />
+    <rect
+      x="80.294"
+      y="367.311"
+      width="344.688"
+      height="148.794"
+      style={{
+        stroke: 'rgb(0, 0, 0)',
+        fillRule: 'nonzero',
+        strokeWidth: '0px',
+        paintOrder: 'fill',
+        fill: 'url(#gradient-2-0)',
+        fillOpacity: '0.13',
+      }}
+      transform="matrix(0.9999999999999999, 0, 0, 0.9999999999999999, 0, -2.842170943040401e-14)"
+    />
+    <rect
+      x="170"
+      y="400"
+      width="160"
+      height="100"
+      style={{
+        strokeLinejoin: 'round',
+        strokeWidth: '8px',
+        paintOrder: 'stroke',
+        fill: 'rgb(33, 33, 35)',
+        stroke: 'rgb(33, 33, 35)',
+      }}
+      transform="matrix(0.9999999999999999, 0, 0, 0.9999999999999999, 0, -2.842170943040401e-14)"
+    />
+    <ellipse
+      style={{
+        fill: 'rgb(216, 216, 216)',
+        stroke: 'rgb(0, 0, 0)',
+        strokeWidth: '0px',
+        transformOrigin: '132.462px 442.827px',
+      }}
+      cx="132.462"
+      cy="442.827"
+      rx="10.075"
+      ry="14.45"
+      transform="matrix(0.438371002674, 0.89879399538, -0.89879399538, 0.438371002674, 0.000033808257, -0.000078576805)"
+    />
+    <ellipse
+      style={{
+        stroke: 'rgb(0, 0, 0)',
+        strokeWidth: '0px',
+        fill: 'rgb(160, 160, 160)',
+        transformOrigin: '123.647px 443.169px',
+      }}
+      cx="123.647"
+      cy="443.169"
+      rx="0.726"
+      ry="2.843"
+      transform="matrix(0.866025090218, 0.499999999999, -0.5, 0.866025090218, 2.403263782893, -0.000216124219)"
+    />
+  </svg>
+)
+
+export default Table
--- src/components/ParallaxView.jsx
+++ src/components/ParallaxView.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import "./ParallaxView.css"
 import AnimationController from "../utils/AnimationController"
 import ScrollProgress from "../utils/ScrollProgress"
+import Table from "../assets/Table"
 
 const ParallaxView = () => {
   const scrollView = useRef(null)
@@ -21,13 +22,117 @@ const ParallaxView = () => {
     }
   }, [scrollView])
 
+  const matrixShower = useRef(null)
+
+  const [counter, setCounter] = useState(0)
+  const [columnCounts, setColumnCounts] = useState([])
+
+  useEffect(() => {
+    const canvas = matrixShower.current
+
+    if (!canvas) return
+
+    const ctx = canvas.getContext("2d")
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // ctx.transform(-1, 0, 0, 1, canvas.width, 0)
+
+    const width = (canvas.width = window.innerWidth)
+    const height = (canvas.height = window.innerHeight)
+
+    const colSize = 20
+    const rowSize = 20
+
+    const cols = Math.floor(width / colSize)
+    const rows = Math.floor(height / rowSize)
+
+    setColumnCounts(
+      new Array(cols).fill(0).map(() => -Math.floor(Math.random() * rows))
+    )
+    const startingColumns = new Array(cols)
+      .fill(0)
+      .map((x) => -Math.floor(Math.random() * rows))
+
+    const chars =
+      "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    const promptText = "SCROLL DOWN"
+    const promptTextStart = Math.floor((cols - promptText.length) / 2)
+    const promptTextEnd = promptTextStart + promptText.length
+    const promptTextRow = Math.floor(rows / 2)
+
+    const draw = () => {
+      ctx.fillStyle = `rgba(0, 8, 0, 0.05)` // this fades the text
+      ctx.fillRect(0, 0, width, height)
+
+      ctx.font = "15pt monospace"
+
+      ctx.textAlign = "center"
+      ctx.textBaseline = "middle"
+
+      // useffect snapshots the state so I must write  logic within the set function. Can i avoid it somehow? I only need the useffect to create the interval.
+      setColumnCounts((currentColumnCounts) => {
+        const newColumnCounts = [...currentColumnCounts]
+
+        for (let i = 0; i < cols; i++) {
+          const count = newColumnCounts[i]
+
+          if (count > rows) {
+            newColumnCounts[i] = -Math.floor(Math.random() * rows)
+            continue
+          }
+
+          const y = count * rowSize
+          const x = colSize / 2 + colSize * i
+
+
+          if (
+            count == promptTextRow &&
+            i >= promptTextStart &&
+            i < promptTextEnd
+          ) {
+            ctx.fillStyle = "#fff"
+            ctx.fillText(promptText[i - promptTextStart], x, y)
+            continue
+          }
+
+          const randomChar = chars[Math.floor(Math.random() * chars.length)]
+
+          // Flip text. Idk why but scaling the context beforehand didn't work for me
+          ctx.save()
+          ctx.translate(x, y)
+          ctx.scale(-1, 1)
+
+          ctx.fillStyle = "#494"
+          ctx.fillText(randomChar, 0, 0)
+
+          ctx.restore()
+        }
+
+        return newColumnCounts.map((c) => c + 1)
+      })
+    }
+
+    const interval = setInterval(() => {
+      requestAnimationFrame(draw)
+    }, 100)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
   return (
     <>
       <div className="view" ref={scrollView}>
-        {/* an image of mountains and a sky with clouds */}
-        <div className="sky" />
-        {/* an image of a person acting as mask */}
+        <canvas ref={matrixShower} className="matrix-shower"></canvas>
+
         <div className="person" />
+        <Table className="table" />
+        {/* an image of a person acting as mask */}
+
+        {/* an image of mountains and a sky with clouds */}
+        {/* <div className="sky" /> */}
         {/* hills */}
         <div className="hills" />
         {/* hills */}
