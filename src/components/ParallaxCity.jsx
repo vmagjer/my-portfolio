6a4dff5ab0decf87246fe14d4ef172d07adefc7d
Vlatko Magjer <vlatko.magjer@gmail.com>
2024-02-25 17:34:57 +0100

    minor adjustments

--- src/components/ParallaxCity.jsx
+++ src/components/ParallaxCity.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react"
+import { useEffect, useRef } from "react"
 import "./ParallaxCity.css"
 import Sun from "../assets/Sun"
 import Moon from "../assets/Moon"
