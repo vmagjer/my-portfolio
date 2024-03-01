f595ad2d6d40efe6c8ecd74d9b8930aa6f74f272
Vlatko Magjer <vlatko.magjer@gmail.com>
2024-03-01 17:58:24 +0100

    move button and menu logic to the menu component

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,90 +1,25 @@
-import PropTypes from 'prop-types'
-import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import SideMenu from './SideMenu'
+import { Outlet } from 'react-router-dom'
 
-
-const menuButtonOffset = 16
-export default function MainLayout({ children }) {
-  const [showNav, setShowNav] = useState(false)
-
-  useEffect(() => {
-    const handleMouseMove = (event) => {
-      controlNav(event)
-      controlButtonAttraction(event)
-    }
-    const controlNav = (event) => {
-      const isWithinThreshold = event.clientX / window.innerWidth < 0.1
-      setShowNav(isWithinThreshold)
-    }
-
-    const controlButtonAttraction = (event) => {
-      const targetX = menuButtonOffset
-      const targetY = window.innerHeight / 2
-      const distanceX = event.clientX - targetX
-      const distanceY = event.clientY - targetY
-      
-      const radius = 800
-      const distance = Math.sqrt(distanceX ** 2 + distanceY ** 2)
-      const progress = 1 - distance / radius
-
-      let translationX = 0
-      let translationY = 0
-      if (progress > 0) {
-        translationX = distanceX * progress
-        translationY = distanceY * progress
-      }
-
-      const button = document.getElementById('attractive-button')
-      button.style.transform = `translateY(calc(-50% + ${translationY}px)) translateX(${translationX}px)`
-    }
-
-    window.addEventListener('mousemove', handleMouseMove)
-
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove)
-    }
-  }, [])
+export default function MainLayout() {
 
   return (
     <Container>
-      <AttractiveButton id="attractive-button">MENU</AttractiveButton>
-      <SideMenu isOpen={showNav} />
+      <SideMenu  />
 
       <Content>
-        {children}
+        <Outlet />
         <Footer>footer</Footer>
       </Content>
     </Container>
   )
 }
 
-MainLayout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
 const Container = styled.div`
   min-height: 100vh;
   width: 100%;
 `
-const AttractiveButton = styled.div`
-  position: fixed;
-  top: 50%;
-  left: ${menuButtonOffset}px;
-  z-index: 1000;
-  transform: translateY(-50%);
-  transition: transform 0.01s;
-
-  padding: 20px;
-  color: white;
-  background-color: black;
-  cursor: pointer;
-
-  width: 60px;
-  height: 60px;
-  border-radius: 10000px;
-`
-
 
 const Content = styled.div``
 
--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,15 +1,41 @@
 import styled from 'styled-components'
-import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react'
 
 const links = [
-  { id: 'home', title: 'Home' },
-  { id: 'about', title: 'About' },
-  { id: 'contact', title: 'Contact' },
+  { id: 'home', title: 'Home', path: '/' },
+  { id: 'about', title: 'About', path: '/about'},
+  { id: 'contact', title: 'Contact', path: '/contact'},
+  { id: 'projects', title: 'Projects', path: '/project'},
 ]
 
-function SideMenu({ isOpen }) {
+function SideMenu() {
+  const [isOpen, setIsOpen] = useState(false)
+  const [isPeeking, setIsPeeking] = useState(false)
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      controlNav(event)
+      controlButtonMovement(event)
+    }
+
+    const controlNav = (event) => {
+      const isWithinThreshold = event.clientX / window.innerWidth < 0.15
+      const isWithinPeekThreshold = event.clientX / window.innerWidth < 0.3
+      setIsPeeking(isWithinPeekThreshold)
+      setIsOpen(isWithinThreshold)
+    }
+
+    window.addEventListener('mousemove', handleMouseMove)
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove)
+    }
+  }, [])
   return (
-    <Container $isOpen={isOpen}>
+    <Wrapper>
+      <AttractiveButton id="attractive-button"></AttractiveButton>
+
+      <Container $isOpen={isOpen} $isPeeking={isPeeking}>
       <Profile>
         <ProfileImage src="https://i.pravatar.cc/300" alt="profile" />
         <span>John Doe</span>
@@ -20,14 +46,74 @@ function SideMenu({ isOpen }) {
         ))}
       </ul>
     </Container>
+    </Wrapper>
   )
 }
 
-SideMenu.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
-};
-
 export default SideMenu
+
+const menuButtonOffset = 100
+const menuButtonSize = 60
+
+
+const controlButtonMovement = (event) => {
+  const distanceFromRight = window.innerWidth - event.clientX
+  const progressX = distanceFromRight / window.innerWidth
+
+  const distanceFromTop = event.clientY
+  const offsetFromMiddle = window.innerHeight / 2 - distanceFromTop
+
+  let translationX = buttonPosition(progressX)
+  let translationY = -offsetFromMiddle * 0.3 * (1 - progressX * 0.5)
+
+  const button = document.getElementById('attractive-button')
+  button.style.transform = `translateY(calc(-50% + ${translationY}px)) translateX(${-translationX}px)`
+}
+
+/**
+ * Button moves towards the edge of the screen, slows significantly, then moves completely to the edge
+ * 
+ * @param {number} progress number between 0 and 1
+ * @returns 
+ */
+const buttonPosition = (progress) => {
+  const slackEnd = 0.6
+  const tensingEnd = 0.7
+  const tenseEnd = 0.7
+  const highlyTenseEnd = 0.85
+
+  const tensingDistance = 40
+  const highlyTensingDistance = 40
+  const giveDistance = menuButtonOffset
+  // slack
+  if (progress < slackEnd) {
+    return 0
+    // tensing
+  } else if (progress < tensingEnd) {
+    return (tensingDistance * (progress - slackEnd)) / (tensingEnd - slackEnd)
+    // tense
+  } else if (progress < tenseEnd) {
+    return 10
+    // highly tensing
+  } else if (progress < highlyTenseEnd) {
+    return (
+      tensingDistance +
+      (highlyTensingDistance * (progress - tenseEnd)) /
+        (highlyTenseEnd - tenseEnd)
+    )
+    // give
+  } else {
+    return giveDistance
+  }
+}
+
+const Wrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: ${menuButtonOffset + menuButtonSize}px;
+`
 
 const Container = styled.div`
   position: fixed;  
@@ -36,15 +122,21 @@ const Container = styled.div`
   width: 300px;
   z-index: 1000;
   
-  transform: translateX(${(props) => (props.$isOpen ? '0' : '-100%')});
+  transform: translateX(
+    ${(props) => {
+      if (props.$isOpen) return '0'
+      if (props.$isPeeking) return '-94%'
+      return '-100%'
+    }}
+  );
   transition: transform 0.3s;
 
   display: flex;
   flex-direction: column;
   
-
-  background: rgba(000 000 000 / 0.3);
-  backdrop-filter: blur(5px);
+  background: #000;
+  border-right: 1px solid #fff;
+  border-radius: 0 8px 8px 0;
 `
 const NavLink = styled.div`
   padding: 10px;
@@ -65,3 +157,20 @@ const ProfileImage = styled.img`
   margin-bottom: 10px;
 `
 
+const AttractiveButton = styled.div`
+  position: fixed;
+  top: 50%;
+  left: ${menuButtonOffset}px;
+  z-index: 1000;
+  transform: translateY(-50%);
+  transition: transform 0.15s;
+
+  padding: 20px;
+  color: white;
+  background-color: black;
+  cursor: pointer;
+
+  width: 60px;
+  height: 60px;
+  border-radius: 10000px;
+`
