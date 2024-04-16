2476672c84df07f7c31c0404f78274930214b020
Vlatko Magjer <vlatko.magjer@gmail.com>
2024-04-16 14:57:48 +0200

    remove unused code

--- src/components/SkillGraph.jsx
+++ src/components/SkillGraph.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import data from '../assets/data'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
 import {
   NetworkDrawingStrategy,
   NodeLinkDiagramRenderer,
@@ -8,25 +8,22 @@ import {
   UndirectedGraph,
 } from '../utils/drawNodeNetwork'
 
-const skillsGraph = new UndirectedGraph(Object.values(data.technologies), (a, b) => {
-  if (a.id === b.id) return 0
-  const commonTagsCount = a.tags.filter(tag => b.tags.includes(tag)).length
-  const aTagsIncludesB = a.tags.includes(b.id) ? 1 : 0
-  const bTagsIncludesA = b.tags.includes(a.id) ? 1 : 0
-  return commonTagsCount + aTagsIncludesB + bTagsIncludesA
-})
+const skillsGraph = new UndirectedGraph(
+  Object.values(data.technologies),
+  (a, b) => {
+    if (a.id === b.id) return 0
+    const commonTagsCount = a.tags.filter((tag) => b.tags.includes(tag)).length
+    const aTagsIncludesB = a.tags.includes(b.id) ? 1 : 0
+    const bTagsIncludesA = b.tags.includes(a.id) ? 1 : 0
+    return commonTagsCount + aTagsIncludesB + bTagsIncludesA
+  }
+)
 
 export default function SkillGraph() {
-  const [R, setR] = useState(0.05)
-
-  function inputChanged(e) {
-    setR(e.target.value)
-  }
-
   const wrapperRef = useRef(null)
   const canvasRef = useRef(null)
+
   useEffect(() => {
-    // initialize canvas
     if (!wrapperRef.current) return
     if (!canvasRef.current) return
     const canvas = canvasRef.current
@@ -37,14 +34,13 @@ export default function SkillGraph() {
     const updatingStrategy = new GraphTopologyStrategy({
       L: 20,
       K_r: 6250,
-      R: R,
-      delta_t: .04, 
+      R: 0.05,
+      delta_t: 0.04,
       max_displacement_squared: 1 ** 2,
-      center: { x: canvas.width / 2, y: canvas.height / 2 }, 
+      center: { x: canvas.width / 2, y: canvas.height / 2 },
       centeringForce: 0.0001,
     })
     const drawingStrategy = new NetworkDrawingStrategy()
-
 
     const network = new NodeLinkDiagramRenderer(
       skillsGraph,
@@ -65,21 +61,20 @@ export default function SkillGraph() {
     return () => {
       cancelAnimationFrame(requestId)
     }
-  }, [wrapperRef, canvasRef, R])
-
-  const onReset = () => {
-    console.log('reset')
-  }
+  }, [wrapperRef, canvasRef])
 
   return (
     <Skills ref={wrapperRef}>
-      <canvas ref={canvasRef} id="skill-graph" width="400" height="400"></canvas>
-      <button onClick={onReset}>Reset</button>
-      <input type="range" min="0" max="0.09" step="0.001" id="myRange" onChange={inputChanged} value={R}></input>
-      {R}
+      <canvas
+        ref={canvasRef}
+        id="skill-graph"
+        width="400"
+        height="400"
+      ></canvas>
     </Skills>
   )
 }
 const Skills = styled.div`
-width: 100%;
-height: 400px;`
+  width: 100%;
+  height: 400px;
+`
