cc5fed78674b7e35d090fc725618c8326ed92d73
Vlatko Magjer <vlatko.magjer@gmail.com>
2024-12-29 00:41:08 +0100

    remove unused code

--- src/assets/chatBubbleTail.tsx
+++
@@ -1,9 +0,0 @@
-export default function chatBubbleTail() {
-  return (
-    <svg viewBox="0 0 40 20" width="40" height="20" fill="black">
-      <clipPath id="tailPath" clipPathUnits="objectBoundingBox">
-        <path d="M 0 20 C 10 20 20 10 20 0 C 20 10 30 20 40 20 Z" />
-      </clipPath>
-    </svg>
-  )
-}
